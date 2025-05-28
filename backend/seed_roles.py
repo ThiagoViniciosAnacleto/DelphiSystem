f471ac4984f4d35664626087796917611782f776
Thiago Anacleto <thiagoanacletonn3@gmail.com>
2025-05-28 11:03:58 -0300

    chore(cruds): adiciona e organiza __init__.py para exportação explícita dos CRUDs
    - Adiciona __init__.py na pasta cruds, importando e expondo explicitamente todas as funções de CRUD de cada entidade
    - Facilita imports organizados e padronizados no projeto
    - Prepara o pacote para uso consistente em toda a aplicação

--- backend/seed_roles.py
+++ backend/seed_roles.py
@@ -1,15 +1,15 @@
 from database import SessionLocal
-import backend.cruds.init as init, schemas
+import backend.cruds as cruds, schemas
 
 def seed_roles():
     db = SessionLocal()
     cargos_iniciais = ["admin", "tecnico", "comum"]
 
     for nome in cargos_iniciais:
-        existing = init.get_role_by_nome(db, nome)
+        existing = cruds.get_role_by_nome(db, nome)
         if not existing:
             role_in = schemas.RoleCreate(nome=nome)
-            init.create_role(db, role_in)
+            cruds.create_role(db, role_in)
             print(f"Criado cargo: {nome}")
         else:
             print(f"Cargo {nome} já existe")
