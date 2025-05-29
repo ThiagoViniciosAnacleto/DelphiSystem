646c85c161f064f9c269f34407a20390a7b4687a
Thiago Anacleto <thiagoanacletonn3@gmail.com>
2025-05-29 17:30:16 -0300

    fix(router): corrigir sintaxe de array de rotas no index.js
    - Corrige erro de sintaxe no arquivo src/router/index.js causado por falta de vírgulas entre objetos
    - Permite que as rotas /recuperar-senha e /resetar-senha sejam registradas corretamente
    - Corrige erro de build no Netlify

--- frontend/suporte-power-frontend/src/router/index.js
+++ frontend/suporte-power-frontend/src/router/index.js
@@ -12,19 +12,19 @@ const routes = [
         path: '/dashboard',
         name: 'Dashboard',
         component: () => import('../views/DashboardView.vue') // criaremos depois
-    }
+    },
 
     {
         path: '/recuperar-senha',
         name: 'RecuperarSenha',
         omponent: () => import('@/views/RecuperarSenhaView.vue')
-    }
+    },
 
     {
         path: '/resetar-senha',
         component: () => import('@/views/ResetarSenhaView.vue')
     }
-    
+
 ]
 
 const router = createRouter({
